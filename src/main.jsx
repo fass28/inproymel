e9a1932a654fbb6853ccebc6cde2b790cfb2221d
freddy saavedra segura <fass28@icloud.com>
2023-06-06 15:13:12 -0500

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { NavBar } from './assets/components/NavBar'
+import { Footer } from './assets/components/Footer'
 
 import './index.css'
-import { NavBar } from './assets/components/NavBar'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <NavBar/>
+    <Footer/>
   </React.StrictMode>,
 )
