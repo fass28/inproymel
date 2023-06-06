5e0138a8625f342e512a8e94de71644baafea6db
freddy saavedra segura <fass28@icloud.com>
2023-06-06 14:51:13 -0500

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import './index.css'
+import { NavBar } from './assets/components/NavBar'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <NavBar/>
+  </React.StrictMode>,
+)
