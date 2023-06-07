a42e91876e54de463f0faa04aba3f9727830ae78
freddy saavedra segura <fass28@icloud.com>
2023-06-06 19:00:40 -0500

    navbar v2 and welcome section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { Footer } from './components/Footer/Footer'
 import { Header } from './components/Header/Header'
+import { Welcome } from './components/Welcome/Welcome'
+import { Footer } from './components/Footer/Footer'
 
 export const App = () => {
   return (
     <>
         <Header/>
+        <Welcome/>
         <Footer/>
     </>
   )
---
+++ src/components/Welcome/Welcome.jsx
@@ -0,0 +1,9 @@
+import "./Welcome.css"
+
+export const Welcome = () => {
+  return (
+    <div className="bg-img">
+        <h1>BIENVENIDOS !</h1>
+    </div>
+  )
+}
