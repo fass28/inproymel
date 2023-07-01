71c061e291db504792115647db85a8f6e310fc2d
freddy saavedra segura <fass28@icloud.com>
2023-07-01 14:22:07 -0500

    size changed

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -8,13 +8,13 @@ import "swiper/css/effect-fade";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
-
 import img_7 from "../../assets/imagenes/welcome-img/img-7.jpg";
 import img_6 from "../../assets/imagenes/welcome-img/img-6.jpg";
 import img_2 from "../../assets/imagenes/welcome-img/img-2.avif";
 import img_9 from "../../assets/imagenes/welcome-img/img-9.jpg";
 import img_12 from "../../assets/imagenes/welcome-img/img-12.jpg";
 import img_13 from "../../assets/imagenes/welcome-img/img-13.jpg";
+import img_10 from "../../assets/imagenes/welcome-img/img-10.jpg";
 
 // import required modules
 import { EffectFade, Navigation, Pagination, Autoplay } from "swiper";
@@ -27,9 +27,10 @@ export const Welcome = () => {
       <div className="welcome-text">
         <h1>Bienvenido</h1>
         <h3>¿Buscando algún Servicio?</h3>
-        <p>En Inproymel estamos para ayudarte y 
-        <br/>
-        brindarte la mejor experiencia con trabajos seguros.
+        <p>
+          En Inproymel estamos para ayudarte y
+          <br />
+          brindarte la mejor experiencia con trabajos seguros.
         </p>
       </div>
       <Swiper
@@ -47,12 +48,27 @@ export const Welcome = () => {
         className="mySwiper"
       >
         <div className="before"></div>
-        <SwiperSlide className="slide-img"><img src={img_7} alt=""  className="welcome-img"/></SwiperSlide>
-        <SwiperSlide className="slide-img"><img src={img_6} alt=""  className="welcome-img"/></SwiperSlide>
-        <SwiperSlide className="slide-img"><img src={img_2} alt="" className="welcome-img" /></SwiperSlide>
-        <SwiperSlide className="slide-img"><img src={img_9} alt=""  className="welcome-img"/></SwiperSlide>
-        <SwiperSlide className="slide-img"><img src={img_12} alt=""  className="welcome-img"/></SwiperSlide>
-        <SwiperSlide className="slide-img"><img src={img_13} alt=""  className="welcome-img"/></SwiperSlide>
+        <SwiperSlide className="slide-img">
+          <img src={img_7} alt="" className="welcome-img" />
+        </SwiperSlide>
+        <SwiperSlide className="slide-img">
+          <img src={img_6} alt="" className="welcome-img" />
+        </SwiperSlide>
+        <SwiperSlide className="slide-img">
+          <img src={img_2} alt="" className="welcome-img" />
+        </SwiperSlide>
+        <SwiperSlide className="slide-img">
+          <img src={img_9} alt="" className="welcome-img" />
+        </SwiperSlide>
+        <SwiperSlide className="slide-img">
+          <img src={img_12} alt="" className="welcome-img" />
+        </SwiperSlide>
+        <SwiperSlide className="slide-img">
+          <img src={img_13} alt="" className="welcome-img" />
+        </SwiperSlide>
+        <SwiperSlide className="slide-img">
+          <img src={img_10} alt="" className="welcome-img" />
+        </SwiperSlide>
         <div className="after"></div>
       </Swiper>
     </section>
