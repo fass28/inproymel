09871edf0c5585b9353778858fde7a8e93de524a
freddy saavedra segura <fass28@icloud.com>
2023-08-07 19:40:42 -0300

    modal drywall update

--- src/components/Services/Modal/ServiceModalAc.jsx
+++ src/components/Services/Modal/ServiceModalAc.jsx
@@ -1,8 +1,29 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import { EffectCoverflow, Pagination, Autoplay } from "swiper";
+
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 
 import ac from "../../../assets/imagenes/services-img/ac.jpg";
-
+import ac_1 from "../../../assets/imagenes/services-img/ac-1.jpg";
+import ac_2 from "../../../assets/imagenes/services-img/ac-2.jpg";
+import ac_3 from "../../../assets/imagenes/services-img/ac-3.jpg";
+import ac_4 from "../../../assets/imagenes/services-img/ac-4.jpg";
+import ac_5 from "../../../assets/imagenes/services-img/ac-5.jpg";
+import ac_6 from "../../../assets/imagenes/services-img/ac-6.jpg";
+import ac_7 from "../../../assets/imagenes/services-img/ac-7.jpg";
+import ac_8 from "../../../assets/imagenes/services-img/ac-8.jpg";
+import ac_9 from "../../../assets/imagenes/services-img/ac-9.jpg";
+import ac_10 from "../../../assets/imagenes/services-img/ac-10.jpg";
+import ac_11 from "../../../assets/imagenes/services-img/ac-11.jpg";
+import ac_12 from "../../../assets/imagenes/services-img/ac-12.jpg";
+import ac_13 from "../../../assets/imagenes/services-img/ac-13.jpg";
+import ac_14 from "../../../assets/imagenes/services-img/ac-14.jpg";
+import ac_15 from "../../../assets/imagenes/services-img/ac-15.jpg";
+import ac_16 from "../../../assets/imagenes/services-img/ac-16.jpg";
 import "./Modal.css";
 
 export const ServiceModalAc = ({ showModal, onHide }) => {
@@ -34,7 +55,81 @@ export const ServiceModalAc = ({ showModal, onHide }) => {
             <li>Visita técnica a empresas y residencias</li>
           </ul>
 
-          <img src={ac} alt="ac ICON" className="services-img" />
+          <div className="img-modal">
+            <Swiper
+              autoplay={{
+                delay: 2000,
+                disableOnInteraction: false,
+              }}
+              effect={"coverflow"}
+              grabCursor={true}
+              centeredSlides={true}
+              slidesPerView={"auto"}
+              loop={true}
+              coverflowEffect={{
+                rotate: 50,
+                stretch: 0,
+                depth: 100,
+                modifier: 1,
+                slideShadows: true,
+              }}
+              modules={[Autoplay, EffectCoverflow, Pagination]}
+              className="mySwiper"
+            >
+              <SwiperSlide>
+                <img src={ac_9} alt="ac ICON" className="services-img" />{" "}
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src={ac_10} alt="ac ICON" className="services-img" />{" "}
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src={ac_11} alt="ac ICON" className="services-img" />{" "}
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src={ac} alt="ac ICON" className="services-img" />{" "}
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src={ac_1} alt="ac ICON" className="services-img" />{" "}
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src={ac_2} alt="ac ICON" className="services-img" />{" "}
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src={ac_3} alt="ac ICON" className="services-img" />{" "}
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src={ac_4} alt="ac ICON" className="services-img" />{" "}
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src={ac_5} alt="ac ICON" className="services-img" />{" "}
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src={ac_6} alt="ac ICON" className="services-img" />{" "}
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src={ac_7} alt="ac ICON" className="services-img" />{" "}
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src={ac_8} alt="ac ICON" className="services-img" />{" "}
+              </SwiperSlide>
+
+              <SwiperSlide>
+                <img src={ac_12} alt="ac ICON" className="services-img" />{" "}
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src={ac_13} alt="ac ICON" className="services-img" />{" "}
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src={ac_14} alt="ac ICON" className="services-img" />{" "}
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src={ac_15} alt="ac ICON" className="services-img" />{" "}
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src={ac_16} alt="ac ICON" className="services-img" />{" "}
+              </SwiperSlide>
+            </Swiper>
+          </div>
         </Modal.Body>
         <Modal.Footer>
           <Button onClick={onHide}>Cerrar</Button>
--- src/components/Services/Modal/ServiceModalDry.jsx
+++ src/components/Services/Modal/ServiceModalDry.jsx
@@ -1,7 +1,24 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import { EffectCoverflow, Pagination, Autoplay } from "swiper";
+
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 
 import drywall from "../../../assets/imagenes/services-img/drywall.jpg";
+import dry_1 from "../../../assets/imagenes/services-img/dry-1.jpg";
+import dry_2 from "../../../assets/imagenes/services-img/dry-2.jpg";
+import dry_3 from "../../../assets/imagenes/services-img/dry-3.jpg";
+import dry_4 from "../../../assets/imagenes/services-img/dry-4.jpg";
+import dry_5 from "../../../assets/imagenes/services-img/dry-5.jpg";
+import dry_6 from "../../../assets/imagenes/services-img/dry-6.jpg";
+import dry_7 from "../../../assets/imagenes/services-img/dry-7.jpg";
+import dry_8 from "../../../assets/imagenes/services-img/dry-8.jpg";
+import dry_9 from "../../../assets/imagenes/services-img/dry-9.jpg";
+import dry_10 from "../../../assets/imagenes/services-img/dry-10.jpg";
+import dry_11 from "../../../assets/imagenes/services-img/dry-11.jpg";
 
 import "./Modal.css";
 
@@ -33,7 +50,66 @@ export const ServiceModalDry = ({ showModal, onHide }) => {
           <li>Pintura de Interioes y exteriores en estructuras Drywall</li>
         </ul>
 
-        <img src={drywall} alt="drywall ICON" className="services-img" />
+        <div className="img-modal">
+          <Swiper
+            autoplay={{
+              delay: 2000,
+              disableOnInteraction: false,
+            }}
+            effect={"coverflow"}
+            grabCursor={true}
+            centeredSlides={true}
+            slidesPerView={"auto"}
+            loop={true}
+            coverflowEffect={{
+              rotate: 50,
+              stretch: 0,
+              depth: 100,
+              modifier: 1,
+              slideShadows: true,
+            }}
+            modules={[Autoplay, EffectCoverflow, Pagination]}
+            className="mySwiper"
+          >
+            <SwiperSlide>
+              <img src={drywall} alt="drywall ICON" className="services-img" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={dry_1} alt="drywall ICON" className="services-img" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={dry_2} alt="drywall ICON" className="services-img" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={dry_3} alt="drywall ICON" className="services-img" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={dry_4} alt="drywall ICON" className="services-img" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={dry_5} alt="drywall ICON" className="services-img" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={dry_6} alt="drywall ICON" className="services-img" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={dry_7} alt="drywall ICON" className="services-img" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={dry_8} alt="drywall ICON" className="services-img" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={dry_9} alt="drywall ICON" className="services-img" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={dry_10} alt="drywall ICON" className="services-img" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={dry_11} alt="drywall ICON" className="services-img" />
+            </SwiperSlide>
+            
+          </Swiper>
+        </div>
       </Modal.Body>
       <Modal.Footer>
         <Button onClick={onHide}>Cerrar</Button>
